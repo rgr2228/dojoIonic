7ded1e594cd29b4426793aa787bb4d8000bb9651
Fabian Higuita <fabianes1996@gmail.com>
2019-07-13 09:27:19 -0500

    dojo ionic listo

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Camera } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,36 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  foto: any;
+
+  constructor(private camera: Camera) {}
+
+  capturarFoto(){
+    this.camera.getPicture({
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      allowEdit: false,
+      saveToPhotoAlbum: true,
+      sourceType: this.camera.PictureSourceType.CAMERA
+    }).then(dataImage =>{
+      this.foto = 'data:image/jpeg;base64,${imageData}' + dataImage;
+    });
+  }
+
+  buscarFoto(){
+    this.camera.getPicture({
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      allowEdit: false,
+      saveToPhotoAlbum: true,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY
+    }).then(dataImage =>{
+      this.foto = 'data:image/jpeg;base64,${imageData}' + dataImage;
+    });
+  }
 
 }
